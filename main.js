61291a201db93239382ac99cdbdd7bb688e1218e
Julian.Antico <julianantico@hotmail.com>
2022-06-21 21:07:40 -0300

    Primer commit pagina JS

---
+++ main.js
@@ -0,0 +1,7 @@
+let nombreUsuario = prompt("Ingrese su nombre");
+
+let apellidoUsuario = prompt("Ingrese su apellido")
+
+let salida = nombreUsuario + " " + apellidoUsuario + " " + "ha sido ingresado";
+
+alert(salida);
