4922ad326ae3f068ba99282b7cacd491d2ac89a5
Julian.Antico <julianantico@hotmail.com>
2022-08-22 20:06:31 -0300

    fetch

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ for (let intentos = 0; intentos <=2; intentos++) {
     if (password === 7890) { 
 
         Swal.fire(
+            'Contraseña Correcta',
             'Felicidades!',
-            'Acertaste la contraseña',
             'success'
         )
         
@@ -23,10 +23,10 @@ for (let intentos = 0; intentos <=2; intentos++) {
     }
 
 }
-
-console.log(carrito);
-document.getElementById("cart-total").innerHTML = localStorage.getItem("totalCarrito") ?? 0
-/* const carrito = []; */
+/* HECHO CON FETCH */
+/* console.log(carrito);
+document.getElementById("cart-total").innerHTML = localStorage.getItem("totalCarrito") ?? 0 */
+/* 
 
 const productos = [
     {id:"1", tittle:"Picador Cogonaut Diablo", price: "$1500", description:"Picador simil cogonaut diablo | Colores a eleccion", img:"../JavaScript Coder/Imagenes/picaDiablo1.jpg"},
@@ -65,9 +65,50 @@ productos.forEach((producto) => {
     document.getElementById(idButton).addEventListener('click', () => {
         carrito.push(producto);
         localStorage.setItem("carrito", JSON.stringify(carrito));
-        document.getElementById("cart-total").innerHTML = carrito.length/*  + Number(totalCarrito) */;
+        document.getElementById("cart-total").innerHTML = carrito.length;
+        console.log(carrito)
+        localStorage.setItem("totalCarrito", JSON.stringify(carrito.length))
+        Swal.fire({
+            position: 'top-center',
+            icon: 'success',
+            title: 'Me Añadiste Al Carrito',
+            showConfirmButton: false,
+            timer: 1200
+          })
+    })
+}) */
+Fetch ("productos.json")
+
+async function LlenarCarrito(){
+    await fetch ("productos.json")
+    .then(response => response.json())
+    .then(j =>{
+    
+    j.forEach((producto) => {
+        const idButton = `add-cart${producto.id}`
+    document.getElementById("seccion-card").innerHTML += `<div class="card">
+    <div class="precio">
+        <p>${producto.price}</p>
+    </div>
+    <img src="${producto.img}">
+    <h4>${producto.tittle}</h4>
+    <p>${producto.description}</p>
+    <a class="boton" id="${idButton}" data.id="${producto.id}">Añadir Al Carrito</a>
+</div>
+`;
+})
+
+j.forEach((producto) => {
+    const idButton = `add-cart${producto.id}`
+    document.getElementById(idButton).addEventListener('click', () => {
+        carrito.push(producto);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        document.getElementById("cart-total").innerHTML = carrito.length;
         console.log(carrito)
         localStorage.setItem("totalCarrito", JSON.stringify(carrito.length))
         alert("Me Agregaste Al Carrito!");
     })
 })
+})
+}
+LlenarCarrito();
