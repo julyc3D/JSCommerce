5a8f2cbd4da0aae565d91ebba3ef708568e9a9ef
Julian.Antico <julianantico@hotmail.com>
2022-08-25 21:00:41 -0300

    final

--- main.js
+++ main.js
@@ -23,10 +23,9 @@ for (let intentos = 0; intentos <=2; intentos++) {
     }
 
 }
-/* HECHO CON FETCH */
-/* console.log(carrito);
-document.getElementById("cart-total").innerHTML = localStorage.getItem("totalCarrito") ?? 0 */
-/* 
+console.log(carrito);
+document.getElementById("cart-total").innerHTML = localStorage.getItem("totalCarrito") ?? 0
+
 
 const productos = [
     {id:"1", tittle:"Picador Cogonaut Diablo", price: "$1500", description:"Picador simil cogonaut diablo | Colores a eleccion", img:"../JavaScript Coder/Imagenes/picaDiablo1.jpg"},
@@ -76,39 +75,4 @@ productos.forEach((producto) => {
             timer: 1200
           })
     })
-}) */
-Fetch ("productos.json")
-
-async function LlenarCarrito(){
-    await fetch ("productos.json")
-    .then(response => response.json())
-    .then(j =>{
-    
-    j.forEach((producto) => {
-        const idButton = `add-cart${producto.id}`
-    document.getElementById("seccion-card").innerHTML += `<div class="card">
-    <div class="precio">
-        <p>${producto.price}</p>
-    </div>
-    <img src="${producto.img}">
-    <h4>${producto.tittle}</h4>
-    <p>${producto.description}</p>
-    <a class="boton" id="${idButton}" data.id="${producto.id}">Añadir Al Carrito</a>
-</div>
-`;
 })
-
-j.forEach((producto) => {
-    const idButton = `add-cart${producto.id}`
-    document.getElementById(idButton).addEventListener('click', () => {
-        carrito.push(producto);
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-        document.getElementById("cart-total").innerHTML = carrito.length;
-        console.log(carrito)
-        localStorage.setItem("totalCarrito", JSON.stringify(carrito.length))
-        alert("Me Agregaste Al Carrito!");
-    })
-})
-})
-}
-LlenarCarrito();
